6824f3beaec81f98cf4b8d73377cdc4f826651de
Guo126 <40754088+Guo126@users.noreply.github.com>
2019-01-15 10:54:47 +0800

    guo

---
+++ Assets/changeMenu.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class changeMenu : MonoBehaviour {
+
+    public Sprite[] menus  ;
+    private int index = 0;
+    public GameObject menu;
+    public GameObject start, world, overlay, hero;
+
+
+	// Use this for initialization
+	void Start () {
+        //if 第一次进入游戏
+        menu.GetComponent<Image>().overrideSprite = menus[0];
+        index = 0;
+        //else{
+        //menu.GetComponent<Image>().overrideSprite = menus[1];
+        //index = 1;
+        //}
+
+
+    }
+    public void theMenu()
+    {
+        if (index == 0)
+        {
+            start.SetActive(false);
+            world.SetActive(true);
+            overlay.SetActive(true);
+            hero.SetActive(true);
+        }
+    }
+	
+	public void nextMenu()
+    {
+        if(index == 3)
+        {
+            index = 0;
+        }
+        else{
+            index += 1;
+        }
+       
+        menu.GetComponent<Image>().overrideSprite = menus[index];
+    }
+
+    public void beforeMenu()
+    {
+        if (index == 0)
+        {
+            index = 3;
+        }
+        else
+        {
+            index -= 1;
+        }
+
+        menu.GetComponent<Image>().overrideSprite = menus[index];
+    }
+
+}
---
+++ Assets/mouseStyle.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class mouseStyle :MonoBehaviour
+{
+    public Texture mouse;
+
+    void Update()
+    {
+        Cursor.visible = false;//隐藏鼠标指针
+    }
+    void OnGUI()
+    {
+        Vector2 msPos=Input.mousePosition;//鼠标的位置
+        GUI.DrawTexture(new Rect(msPos.x, Screen.height-msPos.y,35,35),mouse);
+    }
+}
+
