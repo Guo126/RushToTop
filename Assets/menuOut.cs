c9f921d44f233a1569311050e1ee9e11e6cb009e
Guo126 <40754088+Guo126@users.noreply.github.com>
2019-01-21 11:04:11 +0800

    new

---
+++ Assets/menuOut.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class menuOut : MonoBehaviour {
+
+    private bool isOut = false;
+    public Sprite go ;
+    public Sprite back ;
+
+
+    public void menuControl()
+    {
+        isOut = !isOut;
+        
+        if (isOut)
+        {
+            transform.parent.Translate(new Vector3(0, -50, 0));
+            gameObject.GetComponent<Image>().overrideSprite = back;
+        }
+        else
+        {
+            transform.parent.Translate(new Vector3(0, 50, 0));
+            gameObject.GetComponent<Image>().overrideSprite = go;
+        }
+    }
+}
