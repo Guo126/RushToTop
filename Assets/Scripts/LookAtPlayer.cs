da9938c3de62e18240c99377893969fa84487447
KnightWangLong <17863108336@163.com>
2019-01-21 22:28:55 +0800

    wang Camera

---
+++ Assets/Scripts/LookAtPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtPlayer : MonoBehaviour {
+
+    private Transform playerTransform;
+    private Vector3 targetPosition;
+    private Vector3 offset;
+    private int smooth = 3;
+    
+	void Start () {
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        offset = gameObject.transform.position - playerTransform.position;
+	}
+
+    void LateUpdate() {
+        targetPosition = playerTransform.position + playerTransform.TransformDirection(offset);//
+        gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, targetPosition, Time.deltaTime * smooth);
+        gameObject.transform.LookAt(playerTransform);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    private Animator anim;
+    private int speedID = Animator.StringToHash("Speed");
+    private int isSpeedUpID = Animator.StringToHash("IsSpeedUp");
+    private int horizontalID = Animator.StringToHash("Horizontal");
+    private int speedZID = Animator.StringToHash("SpeedZ");
+    private int speedRotateID = Animator.StringToHash("SpeedRotate");
+
+	// Use this for initialization
+	void Start () {
+        anim = gameObject.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        anim.SetFloat(speedZID, Input.GetAxis("Vertical") * 4.1f);
+        anim.SetFloat(speedRotateID, Input.GetAxis("Horizontal") * 126);
+
+        //anim.SetFloat(speedID, Input.GetAxis("Vertical"));
+        //anim.SetFloat(horizontalID, Input.GetAxis("Horizontal"));
+        //if (Input.GetKeyDown(KeyCode.LeftShift))
+        //{
+        //    anim.SetBool(isSpeedUpID, true);
+        //}
+        //if (Input.GetKeyUp(KeyCode.LeftShift))
+        //{
+        //    anim.SetBool(isSpeedUpID, false);
+        //}
+    }
+}
