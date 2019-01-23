c15d16a3926c367c6ec3811c55ef89defa3e4744
Billy <591243801@qq.com>
2019-01-23 17:37:54 +0800

    move

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityStandardAssets.ImageEffects;
 
 public class Player : MonoBehaviour {
 
@@ -11,16 +13,55 @@ public class Player : MonoBehaviour {
     private int speedZID = Animator.StringToHash("SpeedZ");
     private int speedRotateID = Animator.StringToHash("SpeedRotate");
 
+    private Vector3 direction = Vector3.zero;
+    private Vector3 target;
+    
+    public GameObject perfab;
+    private CharacterController characterController;
+
 	// Use this for initialization
 	void Start () {
         anim = gameObject.GetComponent<Animator>();
+	    characterController = GetComponent<CharacterController>();
+
+	    target = transform.position;
 	}
 	
 	// Update is called once per frame
-	void Update () {
+    void Update()
+    {
 
-        anim.SetFloat(speedZID, Input.GetAxis("Vertical") * 4.1f);
-        anim.SetFloat(speedRotateID, Input.GetAxis("Horizontal") * 126);
+        if (Input.GetMouseButton(1))
+        {
+            LayerMask lm = 1 << LayerMask.NameToLayer("Road");
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Debug.DrawRay(ray.origin, ray.direction, Color.blue);
+            RaycastHit info;
+            Physics.Raycast(ray, out info, 1000f, lm.value);
+            Debug.Log(info.collider.name);
+            GameObject.Instantiate(perfab, info.point, Quaternion.identity);
+
+            target = info.point;
+
+
+        }
+
+        direction = target - transform.position;
+        direction.y = 0;
+        //direction = direction.normalized;
+
+        if (direction.magnitude < 0.3f)
+        {
+            direction = Vector3.zero;
+            target = transform.position;
+
+        }
+
+        target.y = this.transform.position.y;
+        this.transform.LookAt(target);
+        characterController.SimpleMove(direction);
+        anim.SetFloat(speedID, Mathf.Clamp01(Mathf.Abs(direction.x)));
+        //anim.SetFloat(speedRotateID, direction.z * 126);
 
         //anim.SetFloat(speedID, Input.GetAxis("Vertical"));
         //anim.SetFloat(horizontalID, Input.GetAxis("Horizontal"));
